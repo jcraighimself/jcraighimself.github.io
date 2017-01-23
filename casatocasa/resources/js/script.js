819000bd97fe824e8d88e275ce96ef9ed1d9fd9d
jcraighimself <jcraighimself@gmail.com>
2017-01-23 14:29:03 -0600

    Casatocasa Commit #2
    Adding some jquery and MailChimp shit

---
+++ casatocasa/resources/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(
+    function(){
+        $("#buy").click(function () {
+            $("#welcomeDiv").fadeToggle();
+        });
+    });
+
+$(document).ready(
+    function(){
+        $("#rent").click(function () {
+            $("#welcomeDiv").fadeToggle();
+        });
+    });
+
+$(document).ready(
+    function(){
+        $("#sell").click(function () {
+            $("#welcomeDiv").fadeToggle();
+        });
+    });
